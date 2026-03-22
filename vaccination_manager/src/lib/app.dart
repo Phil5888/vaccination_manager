61b3a6f89e49579f27142a9b872fe8d06d4fc381
Phil <spinner.philipp@gmail.com>
2026-03-22 17:05:33 +0100

    theme and language are now taken from system

--- vaccination_manager/src/lib/app.dart
+++ vaccination_manager/src/lib/app.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:vaccination_manager/core/constants/app_theme.dart';
 import 'package:vaccination_manager/l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:vaccination_manager/presentation/navigation/app_router.dart';
-import 'package:vaccination_manager/presentation/providers/settings/settings_providers.dart';
 import 'package:vaccination_manager/presentation/screens/startup/app_startup_gate.dart';
-import 'package:vaccination_manager/presentation/viewmodels/theme_provider.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final theme = ref.watch(themeProvider);
-    final localeCode = ref.watch(settingsProvider).locale.languageCode;
-
     return MaterialApp(
       title: 'Vaccination Manager',
-      theme: theme,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
       onGenerateRoute: AppRouter.generateRoute,
       home: const AppStartupGate(),
-      locale: Locale(localeCode),
       supportedLocales: AppLocalizations.supportedLocales,
       localizationsDelegates: const [AppLocalizations.delegate, GlobalMaterialLocalizations.delegate, GlobalWidgetsLocalizations.delegate, GlobalCupertinoLocalizations.delegate],
     );
