d1247f7be4d537fa0b5802ee6861205eef50517e
Phil <spinner.philipp@gmail.com>
2026-03-29 16:47:08 +0200

    removed unused import

--- src/test/presentation/viewmodels/vaccination_viewmodel_test.dart
+++ src/test/presentation/viewmodels/vaccination_viewmodel_test.dart
@@ -4,10 +4,8 @@ import 'package:vaccination_manager/domain/entities/vaccination_series_status.da
 import 'package:vaccination_manager/presentation/providers/user_providers.dart';
 import 'package:vaccination_manager/presentation/providers/vaccination_dependency_providers.dart';
 import 'package:vaccination_manager/presentation/providers/vaccination_providers.dart';
-import 'package:vaccination_manager/presentation/viewmodels/settings_viewmodel.dart';
 
 import '../../helpers/fakes/fake_active_user_notifier.dart';
-import '../../helpers/fakes/fake_settings_repository.dart';
 import '../../helpers/fakes/fake_vaccination_repository.dart';
 import '../../helpers/fixtures.dart';
 
@@ -19,13 +17,9 @@ void main() {
     final user = activeUserId == 1
         ? Fixtures.userAlice()
         : activeUserId == 2
-            ? Fixtures.userBob()
-            : null;
-    return ProviderContainer(overrides: [
-      activeUserProvider
-          .overrideWith(() => FakeActiveUserNotifier(user)),
-      vaccinationRepositoryProvider.overrideWith((_) => fakeRepo),
-    ]);
+        ? Fixtures.userBob()
+        : null;
+    return ProviderContainer(overrides: [activeUserProvider.overrideWith(() => FakeActiveUserNotifier(user)), vaccinationRepositoryProvider.overrideWith((_) => fakeRepo)]);
   }
 
   setUp(() {
@@ -34,10 +28,7 @@ void main() {
 
   group('VaccinationViewModel', () {
     test('build() with active user returns that user\'s entries', () async {
-      fakeRepo.seedAll([
-        Fixtures.singleShotPast(name: 'Flu', userId: 1),
-        Fixtures.singleShotPast(name: 'MMR', userId: 2),
-      ]);
+      fakeRepo.seedAll([Fixtures.singleShotPast(name: 'Flu', userId: 1), Fixtures.singleShotPast(name: 'MMR', userId: 2)]);
       final container = makeContainer(activeUserId: 1);
       addTearDown(container.dispose);
 
@@ -60,9 +51,7 @@ void main() {
       addTearDown(container.dispose);
       await container.read(vaccinationProvider.future);
 
-      await container
-          .read(vaccinationProvider.notifier)
-          .saveSeries(Fixtures.threeShots(userId: 1));
+      await container.read(vaccinationProvider.notifier).saveSeries(Fixtures.threeShots(userId: 1));
 
       final entries = await container.read(vaccinationProvider.future);
       expect(entries.length, 3);
@@ -76,10 +65,7 @@ void main() {
       await container.read(vaccinationProvider.future);
 
       // Save a 2-shot version of the same vaccine
-      final twoShots = [
-        Fixtures.threeShots(userId: 1).first.copyWith(totalShots: 2),
-        Fixtures.threeShots(userId: 1)[1].copyWith(totalShots: 2),
-      ];
+      final twoShots = [Fixtures.threeShots(userId: 1).first.copyWith(totalShots: 2), Fixtures.threeShots(userId: 1)[1].copyWith(totalShots: 2)];
       await container.read(vaccinationProvider.notifier).saveSeries(twoShots);
 
       final entries = await container.read(vaccinationProvider.future);
@@ -94,9 +80,7 @@ void main() {
       final before = await container.read(vaccinationProvider.future);
       final idToDelete = before.first.id!;
 
-      await container
-          .read(vaccinationProvider.notifier)
-          .deleteShot(idToDelete);
+      await container.read(vaccinationProvider.notifier).deleteShot(idToDelete);
 
       final after = await container.read(vaccinationProvider.future);
       expect(after.length, 2);
@@ -104,17 +88,12 @@ void main() {
     });
 
     test('deleteSeries() removes all shots in the series', () async {
-      fakeRepo.seedAll([
-        ...Fixtures.threeShots(name: 'COVID-19', userId: 1),
-        Fixtures.singleShotPast(name: 'Flu', userId: 1),
-      ]);
+      fakeRepo.seedAll([...Fixtures.threeShots(name: 'COVID-19', userId: 1), Fixtures.singleShotPast(name: 'Flu', userId: 1)]);
       final container = makeContainer(activeUserId: 1);
       addTearDown(container.dispose);
       await container.read(vaccinationProvider.future);
 
-      await container
-          .read(vaccinationProvider.notifier)
-          .deleteSeries(1, 'COVID-19');
+      await container.read(vaccinationProvider.notifier).deleteSeries(1, 'COVID-19');
 
       final entries = await container.read(vaccinationProvider.future);
       expect(entries.length, 1);
@@ -127,9 +106,7 @@ void main() {
       addTearDown(container.dispose);
       await container.read(vaccinationProvider.future);
 
-      await container.read(vaccinationProvider.notifier).saveSeries([
-        Fixtures.singleShotPast(name: 'Tetanus', userId: 1),
-      ]);
+      await container.read(vaccinationProvider.notifier).saveSeries([Fixtures.singleShotPast(name: 'Tetanus', userId: 1)]);
 
       // User 2's shot must still be in the repo
       final user2Entries = await fakeRepo.getVaccinationsForUser(2);
@@ -140,12 +117,7 @@ void main() {
 
   group('seriesListProvider', () {
     test('derives series from vaccination entries', () async {
-      fakeRepo.seedAll(Fixtures.threeShots(
-        userId: 1,
-        d1: Fixtures.thirtyDaysAgo,
-        d2: Fixtures.yesterday,
-        d3: null,
-      ));
+      fakeRepo.seedAll(Fixtures.threeShots(userId: 1, d1: Fixtures.thirtyDaysAgo, d2: Fixtures.yesterday, d3: null));
       final container = makeContainer(activeUserId: 1);
       addTearDown(container.dispose);
 
